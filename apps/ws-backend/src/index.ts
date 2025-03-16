253ef0e229c6e29db0d398df70d23e25a90da74b
TheGaurdiann <rahuldadhich80263@gmail.com>
2025-03-17 01:54:34 +0530

    Add BG steps

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,22 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 const wss = new WebSocketServer({ port: 8080 });
-wss.on("connection",(ws)=>{
+
+
+wss.on("connection",(ws,request)=>{
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(!(decoded as JwtPayload).userId || !decoded){
+        ws.send("Unauthorized");
+        ws.close();
+    }
     ws.on("message",(message)=>{
         ws.send("pong");
     })
