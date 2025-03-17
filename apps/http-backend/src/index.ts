1688197633f9feb8eb06159b304ec391584b711e
TheGaurdiann <rahuldadhich80263@gmail.com>
2025-03-18 00:37:05 +0530

    Updated the Backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,30 +1,52 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from '@repo/backend-common/config';
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {
+  CreateUserSchema,
+  SigninSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient} from "@repo/db/client";
 
 const app = express();
 
 app.post("/signup", async (req, res) => {
-
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
+  res.json({
+    userId: "123",
+  });
 });
 app.post("/signin", async (req, res) => {
-  const userId = 1;
-const token = jwt.sign({userId}, JWT_SECRET);
-  res.json({token});
-});
-app.post("/room",middleware, async (req, res) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
     res.json({
-        message: "Room created",
+      message: "Incorrect Inputs",
     });
+    return;
+  }
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({ token });
 });
-
-
-
-
-
-
-
-
-const port = 3000;
-app.listen(port);
+app.post("/room", middleware, async (req, res) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
+  res.json({
+    roomId: 123,
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
